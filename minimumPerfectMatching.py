76242b0904be1ae8292d818e8b56d5431d830aa3
zshureih <42466551+zshureih@users.noreply.github.com>
2020-03-08 21:26:46 -0700

    fix/removed dead code

--- minimumPerfectMatching.py
+++ minimumPerfectMatching.py
@@ -2,38 +2,6 @@ import math
 import sys
 import queue
 
-def d(u, v):
-    return round(math.sqrt((u[0]-v[0])*(u[0]-v[0])+(u[1]-v[1])*(u[1]-v[1])), 0)
-
-class subgraph:
-    def __init__(self, n, points, G):
-        self.n = n
-        self.adjList = {}
-        self.key = {}
-        self.points = points
-        self.populate_edges(G)
-
-    def populate_edges(self, G):
-        edges = {}
-        visited = []
-        for k in range(0, len(self.points)):      # k = u pointer
-            if not self.points[k] in edges.keys():
-                edges[self.points[k]] = []
-            for l in range(0, len(self.points)):  # l = v pointer
-                if not self.points[l] in edges.keys():
-                    edges[self.points[l]] = []
-                u = [G.xCoord[self.points[k]], G.yCoord[self.points[k]]] # coords of node 1
-                v = [G.xCoord[self.points[l]], G.yCoord[self.points[l]]] # coords of node 2
-                if not k == l and not [k,l] in visited and not [l,k] in visited: # if we aren't comparing to self and we haven't already gone through u
-                    w = d(u,v) 
-                    edges[self.points[k]].append([l, w])
-                    edges[self.points[l]].append([k, w])
-                    visited.append([k,l])
-                    visited.append([l,k])
-            self.key[self.points[k]] = u
-        self.adjList = edges
-
-
 def generateOddVertices(G):
 
     # get degrees of each node in the MST
